0e7ef3f6446bd31a1ff7ffd08e5cdb2611e06fe0
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-10-08 15:38:14 +0200

    chore(ourProject)

---
+++ ourProject/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+  public static void main(String[] args) {
+    shoutHelloWorld();
+  }
+
+  public static void shoutHelloWorld() {
+    System.out.println("Hello world!");
+  }
+
+}
