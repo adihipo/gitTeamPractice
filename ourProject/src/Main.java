ea1bd8534e5d59b46143c24f9542c19ede60d80d
khajnal <hajnal.krisztina03@gmail.com>
2018-10-08 15:48:48 +0200

    void method, what prints my age created

--- ourProject/src/Main.java
+++ ourProject/src/Main.java
@@ -2,10 +2,14 @@ public class Main {
 
   public static void main(String[] args) {
     shoutHelloWorld();
+    printBAge();
   }
 
   public static void shoutHelloWorld() {
     System.out.println("Hello world!");
   }
 
+  public static void printBAge() {
+    System.out.println(23);
+  }
 }
