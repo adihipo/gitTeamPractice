eaab37f25854f02a0413a42a8f3ae92f9986e3ae
khajnal <hajnal.krisztina03@gmail.com>
2018-10-08 16:06:22 +0200

    b

--- ourProject/src/Main.java
+++ ourProject/src/Main.java
@@ -2,25 +2,17 @@ public class Main {
 
   public static void main(String[] args) {
     shoutHelloWorld();
-<<<<<<< HEAD
-    printBAge();
-=======
+    printBAge(23);
     cheer();
->>>>>>> 694c9971f46bd6d5b25d59250badc7efb5d5b614
   }
 
   public static void shoutHelloWorld() {
     System.out.println("Hello world!");
   }
-
-<<<<<<< HEAD
-  public static void printBAge() {
-    System.out.println(23);
+  public static void printBAge(int i) {
+    System.out.println(i);
   }
-=======
   public static void cheer(){
     System.out.println("Happy Hypos & Barbie (sort of)");
   }
-
->>>>>>> 694c9971f46bd6d5b25d59250badc7efb5d5b614
 }
