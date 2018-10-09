03268f3f8e2e8054e1244d3038e3da6610e25734
khajnal <hajnal.krisztina03@gmail.com>
2018-10-09 10:40:12 +0200

    B6

--- ourProject/src/Cheers.java
+++ ourProject/src/Cheers.java
@@ -1,8 +1,13 @@
 public class Cheers {
 
-  private String baseLanguage = "java";
 
-  public void cheers (){
-    System.out.println(baseLanguage);
+  private String baseLanguage = "java";
+  private String languageLevel = "basic";
+
+  private String baseLanguage = "lennazutcáááánnincsen kegyelem, len az utszan nincsen szerelemmmmmm";
+
+
+  public void cheers() {
+    System.out.println(languageLevel + baseLanguage);
   }
 }
