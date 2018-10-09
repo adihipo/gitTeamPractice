9312690996a03422ecf0629fdebc6bfc34a61837
Laszlo Daru <daruu@hotmail.com>
2018-10-09 10:41:16 +0200

    d8

--- ourProject/src/Cheers.java
+++ ourProject/src/Cheers.java
@@ -4,10 +4,11 @@ public class Cheers {
   private String baseLanguage = "java";
   private String languageLevel = "basic";
 
-  private String baseLanguage = "lennazutcáááánnincsen kegyelem";
+  private String baseLanguage1 = "lennazutcáááánnincsen kegyelem";
 
 
   public void cheers() {
     System.out.println(languageLevel + baseLanguage);
+    System.out.println(baseLanguage1);
   }
 }
