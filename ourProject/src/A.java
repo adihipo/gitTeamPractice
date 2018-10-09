f19c51f9802d5f06a9ccf449e60429cc3959ba2e
Laszlo Daru <daruu@hotmail.com>
2018-10-09 10:43:46 +0200

    d9

--- ourProject/src/A.java
+++ ourProject/src/A.java
@@ -1,9 +1,11 @@
 public class A {
 
   private Long dick;
+  public String morning = "Good Morning TeaPot1";
 
   public void greet() {
     System.out.println("Greetings");
+    System.out.println(morning);
   }
 
   public void shoutDick() {
